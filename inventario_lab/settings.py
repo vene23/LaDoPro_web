2e63221d7388b3bbb4b02646fad7e26b6bb12acd
venecia calandron <vene_23@hotmail.com>
2025-11-12 10:23:00 -0300

    Add STATICFILES_STORAGE for WhiteNoise

--- inventario_lab/settings.py
+++ inventario_lab/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -155,3 +156,5 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Imprime rutas estáticas para depuración
 print("STATICFILES_DIRS:", STATICFILES_DIRS)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
