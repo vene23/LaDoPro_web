c871e23d976233250b4998f752b31bbe8c4c112b
venecia calandron <vene_23@hotmail.com>
2025-11-14 08:10:03 -0300

    Cambio backend de correo a console.EmailBackend para testear en Render

--- inventario_lab/settings.py
+++ inventario_lab/settings.py
@@ -142,7 +142,7 @@ LOGIN_URL = '/login/'  # Ruta a la vista de inicio de sesión
 LOGIN_REDIRECT_URL = '/inventario/dashboard/'
 
 # Configuración de correo real
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
