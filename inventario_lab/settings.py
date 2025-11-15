3c5f7abf9381234b315f5586d81f465bd3b7aea1
venecia calandron <vene_23@hotmail.com>
2025-11-15 16:50:36 -0300

    Enable Whitenoise static files for Railway

--- inventario_lab/settings.py
+++ inventario_lab/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,7 +157,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
