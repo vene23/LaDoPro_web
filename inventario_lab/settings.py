6efe14eed6a0ed63321ea7f9620591a0b0ef2967
venecia calandron <vene_23@hotmail.com>
2025-11-16 15:00:01 -0300

    Remove sqlite local file

--- inventario_lab/settings.py
+++ inventario_lab/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config  # Biblioteca para cargar variables del archivo .env
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,10 +90,11 @@ WSGI_APPLICATION = 'inventario_lab.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
