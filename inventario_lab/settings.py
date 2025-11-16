705eeeb6b78349822e3e06dc167fd97c6da665ae
venecia calandron <vene_23@hotmail.com>
2025-11-16 15:03:09 -0300

    Configuración PostgreSQL

--- inventario_lab/settings.py
+++ inventario_lab/settings.py
@@ -90,10 +90,9 @@ WSGI_APPLICATION = 'inventario_lab.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
         conn_max_age=600,
-        ssl_require=True
     )
 }
 
